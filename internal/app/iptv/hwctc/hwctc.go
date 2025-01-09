15b4e01bed2ca451e870e00368bf90aec23ab3b7
superant <darkstsuper321@gmail.com>
2025-01-09 22:33:12 +0800

    feat: 支持自定义配置HTTP请求头。

--- internal/app/iptv/hwctc/hwctc.go
+++ internal/app/iptv/hwctc/hwctc.go
@@ -1,6 +1,7 @@
 package hwctc
 
 import (
+	"fmt"
 	"iptv/internal/app/iptv"
 	"net/http"
 
@@ -8,25 +9,41 @@ import (
 )
 
 type Client struct {
-	httpClient *http.Client // HTTP客户端
-	config     *Config      // IPTV配置
-	host       string       // 缓存最新重定向的服务器地址和端口
+	httpClient *http.Client      // HTTP客户端
+	config     *Config           // hwctc相关配置
+	key        string            // 加密Authenticator的秘钥
+	originHost string            // HTTP请求的服务器地址端口
+	headers    map[string]string // 自定义HTTP请求头
+
+	host string // 缓存最新重定向的服务器地址和端口
 
 	logger *zap.Logger // 日志
 }
 
 var _ iptv.Client = (*Client)(nil)
 
-func NewClient(httpClient *http.Client, config *Config) (iptv.Client, error) {
-	// 校验config配置
-	if err := config.Validate(); err != nil {
+func NewClient(httpClient *http.Client, config *Config, key, serverHost string, headers map[string]string) (iptv.Client, error) {
+	// config不能为空
+	if config == nil {
+		return nil, fmt.Errorf("client config is nil")
+	} else if err := config.Validate(); err != nil { // 校验config配置
 		return nil, err
+	}
+
+	// 密钥和服务器地址必须配置
+	if key == "" {
+		return nil, fmt.Errorf("key is empty")
+	} else if serverHost == "" {
+		return nil, fmt.Errorf("serverHost is empty")
 	}
 
 	i := Client{
 		httpClient: httpClient,
-		host:       config.ServerHost,
 		config:     config,
+		key:        key,
+		originHost: serverHost,
+		headers:    headers,
+		host:       serverHost,
 		logger:     zap.L(),
 	}
 	if i.httpClient == nil {
@@ -37,10 +54,10 @@ func NewClient(httpClient *http.Client, config *Config) (iptv.Client, error) {
 
 func (c *Client) setCommonHeaders(req *http.Request) {
 	req.Header.Set("Host", c.host)
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; Fhbw2.0) AppleWebKit/534.24 (KHTML, like Gecko) Safari/534.24 chromium/webkit")
-	req.Header.Set("Accept-Language", "zh-CN,en-US;q=0.8")
-	if c.config.XRequestedWith != "" {
-		req.Header.Set("X-Requested-With", c.config.XRequestedWith)
+	// 设置自定义HTTP请求头
+	if len(c.headers) > 0 {
+		for k, v := range c.headers {
+			req.Header.Set(k, v)
+		}
 	}
 }
