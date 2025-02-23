988517a8e0196ac2a865e5c6f91080d895ad724d
superant <darkstsuper321@gmail.com>
2025-02-24 00:28:31 +0800

    fix: 直播配置内容支持灵活配置

--- internal/app/router/config_controller.go
+++ internal/app/router/config_controller.go
@@ -11,17 +11,7 @@ type Lives struct {
 }
 
 // Live 直播配置
-type Live struct {
-	Name    string `json:"name"`              // 配置名称
-	Boot    bool   `json:"boot"`              // 是否自启动
-	Url     string `json:"url"`               // 直播源地址
-	Epg     string `json:"epg,omitempty"`     // 节目地址
-	Logo    string `json:"logo,omitempty"`    // 台标地址
-	Pass    bool   `json:"pass,omitempty"`    // 是否免密码
-	Ua      string `json:"ua,omitempty"`      // 用户代理
-	Origin  string `json:"origin,omitempty"`  // 来源
-	Referer string `json:"referer,omitempty"` // 参照地址
-}
+type Live map[string]any
 
 var lives *Lives
 
@@ -38,5 +28,5 @@ func GetLivesConfig(c *gin.Context) {
 	}
 
 	// 返回响应
-	c.JSON(http.StatusOK, lives)
+	c.PureJSON(http.StatusOK, lives)
 }
